34dc9515dc1d60572ab1c0912acd546b61955600
xyzsid <sid.dommeti@gmail.com>
2021-10-12 11:03:51 -0500

    working Hulu Web

---
+++ utils/requests.js
@@ -0,0 +1,48 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+  Trending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  TopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  Action: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
+  Comedy: {
+    title: "Comedy",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  Horror: {
+    title: "Horror",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  Romance: {
+    title: "Romance",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  Mystery: {
+    title: "Mystery",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
+  ScienceFiction: {
+    title: "Science Fiction",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  },
+  Western: {
+    title: "Western",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  },
+  Animated: {
+    title: "Animation",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  },
+  Television: {
+    title: "Made for TV Movies",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  },
+};
